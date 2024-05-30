e082daff3dd8218aa5287269cf88de38590a4a5a
Charles <creidbrowniii@gmail.com>
2024-05-30 02:18:09 -1000

    ui not rendering :(

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("#todo-form");
+const list = document.querySelector("#todo-list");
+
+if (localStorage["data"] !== null && localStorage["data"] !== undefined) {
+    todos = JSON.parse(localStorage["data"]);
+}
+
+function buildUI() {
+    let HTML = ``;
+    todos.forEach((todo) => {
+        HTML += `
+            <li id="${todo.id}">
+                ${todo.title}
+                <button aria-label="Complete" class="button-complete>
+                    X
+                </button>
+            </li>`;
+    });
+    list.innerHTML = HTML;
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // add to-do to data and render UI
+    let todos = [];
+
+
+    todos.push({
+        title: event.target[0].value,
+        complete: false, // item isn't complete yet
+        id: self.crypto.randomUUID() // gives each list item unique identifiers!
+    });
+
+    // update data
+    localStorage["data"] = JSON.stringify(todos);
+    buildUI();
+    form.reset();
+})
+
+document.documentElement.addEventListener("click", (event) => {
+    if (event.target.classList.contains("button-complete")) {
+        todos = todos.filter((todo) => todo.id !== event.target.parentElement.id);
+        localStorage["data"] = JSON.stringify(todos);
+        buildUI();
+    }
+});
+
+buildUI();
